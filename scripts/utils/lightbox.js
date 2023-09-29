b75b334ba8ed9bc13a51106fe4da72f5ba97d562
zerossama <charlotte@anros.fr>
2023-09-29 14:25:35 +0200

    lightbox css done

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,7 +1,9 @@
 export function displayLightbox(e) {
-    const media = document.querySelector("#media");
+    const mainPage = document.querySelector(".main_page");
+    mainPage.style.display = "none";
+    const media = document.querySelector(".media");
     const lightboxElement = document.querySelector(".lightbox");
-    lightboxElement.style.display = "block";
+    lightboxElement.style.display = "flex";
 
     const lightboxImg = document.createElement("img");
     lightboxImg.setAttribute("src", e.currentTarget.src);
@@ -11,6 +13,8 @@ export function displayLightbox(e) {
 }
 
 export function closeLightbox() {
+    const mainPage = document.querySelector(".main_page");
+    mainPage.style.display = "block";
     const lightbox = document.querySelector(".lightbox");
     lightbox.style.display = "none";
 }
